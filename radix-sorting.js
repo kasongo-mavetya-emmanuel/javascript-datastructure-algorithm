cb961309db3dbd734483f5a8627e1bc1a0d49c13
unknown <emmanuelkasongomavetya@gmail.com>
2023-11-08 07:52:42 +0200

    radix sorting tested

--- radix-sorting.js
+++ radix-sorting.js
@@ -1,26 +1,36 @@
 function getDigit(num, i){
-
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
 function countDigit(num){
-
+   if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num)))+1;
 }
 
-function mostDigit(){
+function mostDigits(arr){
 
-    return maxDigit;
+    let maxDigits=0;
+    for(let i=0; i<arr.length; i++){
+      maxDigits = Math.max(maxDigits,countDigit(arr[i]));
+    
+    }
+ 
+    return maxDigits;
 }
 
 
 function radixSorting(arr){
-    const k= mostDigit(arr);
-    const buckets= Array.from({length:10},[]);
-    for(let i=0; i<=k ; k++){
+    const maxDigits= mostDigits(arr);
+    for(let i=0; i<=maxDigits ; i++){
+      const buckets= Array.from({length:10},()=>[]);
         for(let j=0; j<arr.length; j++){
             let digit= getDigit(arr[j],i);
              buckets[digit].push(arr[j]);
         }
-        buckets=[].concat(...buckets);
-
+        arr=[].concat(...buckets);
     }
+    return arr;
 }
+
+
+console.log(radixSorting([1245,3,433,2222,12,31,20,5,9,7,6,1]));
