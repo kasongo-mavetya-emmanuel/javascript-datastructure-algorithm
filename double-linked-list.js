18413e4d94061b57c9a84978cd67f6d653aed756
unknown <emmanuelkasongomavetya@gmail.com>
2023-11-21 12:42:35 +0200

    DLL pop method added and push modified

--- double-linked-list.js
+++ double-linked-list.js
@@ -14,15 +14,37 @@ class DoubleLinkedList{
     }
     push(val){
         let newNode=new Node(val);
-        if(!this.head){
+        if(!this.length===0){
             this.head=newNode;
             this.tail=newNode;
+            
         }else{
             this.tail.next=newNode;
             newNode.prev=this.tail;
             this.tail=newNode;
         }
+        this.length++;
+
+        return this;
     }
+    pop(){
+        if(!this.head) return undefined;
+        let poppedNode= this.tail;
+        if(this.length===1){
+            this.head=null;
+            this.tail=null; 
+        }
+        else{
+            this.tail=poppedNode.prev;
+            this.tail.next=null;
+            poppedNode.prev=null;
+           
+        }
+        return poppedNode;
+
+    }
+    
+
 }
 
  const list= new DoubleLinkedList();
