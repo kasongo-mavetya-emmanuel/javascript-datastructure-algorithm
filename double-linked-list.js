ad5437c0a20b55ab8e834cbe03aa81a014c395d4
unknown <emmanuelkasongomavetya@gmail.com>
2023-11-21 12:33:38 +0200

    push method in DLL added

--- double-linked-list.js
+++ double-linked-list.js
@@ -12,4 +12,36 @@ class DoubleLinkedList{
         this.tail=null;
         this.length=0;
     }
+    push(val){
+        let newNode=new Node(val);
+        if(!this.head){
+            this.head=newNode;
+            this.tail=newNode;
+        }else{
+            this.tail.next=newNode;
+            newNode.prev=this.tail;
+            this.tail=newNode;
+        }
+    }
 }
+
+ const list= new DoubleLinkedList();
+
+list.push('hi');
+list.push('you');
+list.push(99);
+list.push('great');
+//list.unshift('new');
+
+//list.pop();
+//list.shift();
+//list.unshift('new');
+//console.log(list.get(2));
+//console.log(list.set('get',2));
+//console.log(list.insert(0,'again'));
+// console.log(list.remove(5));
+//list.reverse();
+//list.print();
+
+
+console.log(list);
