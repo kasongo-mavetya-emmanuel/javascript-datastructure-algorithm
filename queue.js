99c7b7af179ec5b2f1a01094bf38607430eeb16d
unknown <emmanuelkasongomavetya@gmail.com>
2023-12-01 21:37:07 +0200

    queue class added

---
+++ queue.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(value){
+        this.val=this.value;
+        this.next=null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first=null;
+        this.last=null;
+        this.size=0;
+    }
+}
+
+const queue= new Queue();
