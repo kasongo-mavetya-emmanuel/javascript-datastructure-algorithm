cd4c83dfe7062f4b637bd734d9253bc4a4243641
unknown <emmanuelkasongomavetya@gmail.com>
2023-12-05 21:51:03 +0200

    BST insert method added

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,4 +11,33 @@ class BinarySearchTree{
     constructor(){
         this.root=null;
     }
+    insert(value){
+        const newNode= new Node(value);
+        if(this.root===null){
+            this.root=newNode;
+            return this;
+        }
+        let current= this.root;
+        while(true){
+            if(value===current.value) return undefined;
+            if(value<current.value){
+                if(current.left===null){
+                    current.left=newNode;
+                    return this;
+                }
+                current= current.left;
+
+            }else{
+                if(current.right===null){
+                    current.right=newNode;
+                    return this;
+                }
+                current=current.right;
+
+            }
+        }
+
+    }
 }
+
+const tree= new BinarySearchTree();
