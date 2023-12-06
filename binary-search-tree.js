f861197c6b0976772c51797b7fdcb84ead0b7318
unknown <emmanuelkasongomavetya@gmail.com>
2023-12-06 20:53:47 +0200

    contains BST method added

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -54,6 +54,21 @@ class BinarySearchTree{
         if(!found)return undefined;
         return current;
     }
+    find(value){
+        if(this.root===null) return false;
+        let current=this.root, found=false;
+        while(current && !found){
+            if(value<current.value){
+              current=current.left;
+            }else if(value>current.value){
+              current= current.right;
+            }else{
+                return true;
+
+            }
+        }
+        return false;
+    }  
 }
 
 const tree= new BinarySearchTree();
