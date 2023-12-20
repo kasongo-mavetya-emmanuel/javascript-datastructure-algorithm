478e3a398710bd91246d13ebb65dcfc0c79cebf8
unknown <emmanuelkasongomavetya@gmail.com>
2023-12-20 21:28:10 +0200

    DSF post order method added and tested traversal tree

--- traversal-tree.js
+++ traversal-tree.js
@@ -96,6 +96,16 @@ class BinarySearchTree{
         traverse(this.root);
         return data;
     }
+    DSFpostOrder(){
+        let data=[];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 const tree= new BinarySearchTree();
@@ -116,3 +126,4 @@ console.log(tree);
 
 console.log(tree.BSF());
 console.log(tree.DSFpreOrder())
+console.log(tree.DSFpostOrder())
