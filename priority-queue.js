d5bfcf181bda76285ebebdaebc9346071f75ad50
unknown <emmanuelkasongomavetya@gmail.com>
2024-01-02 19:36:51 +0200

    dequeue method in Priority queue added

--- priority-queue.js
+++ priority-queue.js
@@ -29,6 +29,45 @@ class PriorityQueue{
 
         }
     }
+
+    dequeue(){
+        let max=this.values[0];
+        let end= this.values.pop();
+        if(this.values.length>0){
+            this.values[0]=end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown(){
+        let idx=0;
+        let length= this.values.length;
+        let element=this.values[0];
+        while(true){
+            let leftChildIdx=2*idx+1;
+            let rightChildIdx=2*idx+2;
+            let leftChild,rightChild,
+            swap=null;
+            if(leftChildIdx<length){
+                leftChild= this.values[leftChildIdx];
+                if(leftChild.priority<element.priority){
+                    swap=leftChildIdx;
+                }
+            }
+            if(rightChildIdx<length){
+                rightChild=this.values[rightChildIdx];
+                if((swap==null && rightChild.priority<element.priority)||
+                (swap!==null && rightChild.priority<leftChild.priority)){
+                    swap=rightChildIdx;
+                }
+            }
+            if(swap===null) break;            
+            this.values[idx]=this.values[swap];
+            this.values[swap]= element;
+            idx=swap;
+        }
+    }
 }
 const prQueue= new PriorityQueue();
 prQueue.enqueue("common cold",5)
