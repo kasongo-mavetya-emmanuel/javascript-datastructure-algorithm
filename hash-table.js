b3e8e981952efb78b4b1561ed21931dcafec0a6e
unknown <emmanuelkasongomavetya@gmail.com>
2024-01-04 10:48:56 +0200

    hash table get keys method added

--- hash-table.js
+++ hash-table.js
@@ -35,6 +35,19 @@ class HashTable{
         return undefined;
     }
 
+    keys(){
+        let keysArr=[];
+        for(let i=0; i<this.keyMap.length;i++){
+            if(this.keyMap[i]){
+                for(let j=0; j<this.keyMap[i].length;j++){
+                    if(!keysArr.includes(this.keyMap[i][j][0])){
+                        keysArr.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+    }
+
 }
 const ht= new HashTable();
 ht.set("maroon","#800000")
