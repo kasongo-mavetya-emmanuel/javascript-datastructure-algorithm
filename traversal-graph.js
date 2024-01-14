335421f0a58138a3d2ba47762073ce714be6388c
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2024-01-14 13:01:22 -0800

    traversal graph depthfirstrecursive method added

---
+++ traversal-graph.js
@@ -0,0 +1,65 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+
+    }
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+
+    }
+
+    depthFirstRecursive(start) {
+        let result = [];
+        let visited = {};
+        const adjacencyList = this.adjacencyList;
+        (function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            adjacencyList[vertex].forEach(neighbour => {
+                if (!visited[neighbour]) {
+                    return dfs(neighbour);
+                }
+
+            });
+        })(start);
+        return result;
+
+    }
+
+}
+
+const g = new Graph();
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+g.depthFirstRecursive("A")
