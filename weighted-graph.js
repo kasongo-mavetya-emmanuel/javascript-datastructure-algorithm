89cdef4a9de6132c10454b1d5a2a8f86331e51e7
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2024-01-16 12:38:38 -0800

    weighted graph add vertex and edge method added

--- weighted-graph.js
+++ weighted-graph.js
@@ -6,6 +6,9 @@ class WeightedGraph {
         if (!this.adjacenyList[vertex]) this.adjacenyList[vertex] = [];
     }
     addEdge(vertex1, vertex2, weight) {
+        this.adjacenyList[vertex1].push({ node: vertex2, weight });
+        this.adjacenyList[vertex2].push({ node: vertex1, weight });
+
 
     }
 }
