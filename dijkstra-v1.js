41f2ad23b4d20ab6f9b935d5861512026f7bd659
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2024-01-18 11:16:26 -0800

    dijkstra v1 method tested

--- dijkstra-v1.js
+++ dijkstra-v1.js
@@ -8,7 +8,7 @@ class PriorityQueue {
 
     }
     dequeue() {
-        this.values.shift();
+        return this.values.shift();
     }
     sort() {
         this.values.sort((a, b) => a.priority - b.priority);
@@ -27,7 +27,7 @@ class WeightedGraph {
         this.adjacenyList[vertex2].push({ node: vertex1, weight });
     }
 
-    Djikstra(start, finish) {
+    Dijkstra(start, finish) {
         const nodes = new PriorityQueue();
         let distances = {};
         let previous = {};
@@ -37,14 +37,65 @@ class WeightedGraph {
         for (let vertex in this.adjacenyList) {
             if (vertex === start) {
                 distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
 
             } else {
                 distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
             }
             previous[vertex] = null;
+        }
+
+        while (nodes.values.length) {
+            smallest = nodes.dequeue().val;
+            if (smallest === finish) {
+                while (previous[smallest]) {
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+
+            if (smallest || distances[smallest] !== Infinity) {
+                for (let neighbor in this.adjacenyList[smallest]) {
+                    const nextNode = this.adjacenyList[smallest][neighbor];
+                    const candidate = distances[smallest] + nextNode.weight;
+                    const nextNeighbor = nextNode.node;
+
+                    if (candidate < distances[nextNeighbor]) {
+                        distances[nextNeighbor] = candidate;
+                        previous[nextNeighbor] = smallest;
+                        nodes.enqueue(nextNeighbor, candidate);
+                    }
+
+                }
+
+            }
 
 
         }
 
+        return path.concat(smallest).reverse();
+
     }
 }
+
+var graph = new WeightedGraph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
+
+
+console.log(graph.Dijkstra("A", "E"));
