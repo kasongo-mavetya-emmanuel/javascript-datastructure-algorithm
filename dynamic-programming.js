e6f21a8d10c2459a23cdaa4f72da304075283fde
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2024-01-24 09:03:18 -0800

    dynamic programming fibonacci with table

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -7,4 +7,16 @@ function fb_memo(n, memo = []) {
     return res;
 }
 
-console.log(fb_memo(10));
+// console.log(fb_memo(10));
+
+function fb_table(n) {
+    if (n <= 2) return 1;
+    let fbNums = [0, 1, 1];
+    for (let i = 3; i <= n; i++) {
+        fbNums[i] = fbNums[i - 1] + fbNums[i - 2];
+    }
+
+    return fbNums[n];
+}
+
+console.log(fb_table(100));
